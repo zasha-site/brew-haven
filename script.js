0751efc21789c808074c9c7113c9403f75319d08
Habeba Ahmed <habiba.mohamed.24030124@iu.edu.eg>
2026-07-06 07:40:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     /* ---------- Playful "pop" animation on any image click ---------- */
-    document.querySelectorAll(".image-box, .space-photo, .service-media").forEach(el => {
+    document.querySelectorAll(".space-photo, .service-media").forEach(el => {
         el.addEventListener("click", (e) => {
             if (e.target.closest(".fav")) return;
             el.classList.remove("img-pop");
